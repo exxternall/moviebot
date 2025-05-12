d2efc4306731883698b404df56438a7826ec2af8
external <88947596+exxternall@users.noreply.github.com>
2025-05-12 20:18:11 +0300

    Updated BOT_TOKEN in config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import Bot
 from typing import Optional
 
-BOT_TOKEN = "8047000749:AAGKR917jH1jr85tsZDUDogKdINNByRz3qY"
+BOT_TOKEN = "8080166792:AAHGb-p7tIns6qFG8pRBDcZq4gP56MI_OS4"
 TMDB_API_KEY = "e56533e6144245a6bd2bccbc5da62329"
 
 bot = Bot(token=BOT_TOKEN)
